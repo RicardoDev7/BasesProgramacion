7598d91e2600cdaa42a2a752a0a7674dc593bf6a
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-16 18:28:36 -0500

    feat: switch

--- sections/2.control-structure.js
+++ sections/2.control-structure.js
@@ -12,5 +12,32 @@
         console.log('El alumno debe estudiar más');
     else
         console.log('El alumno reprobo el curso');
+    var weekDay = 3;
+    switch (weekDay) {
+        case 1:
+            console.log('Lunes');
+            break;
+        case 2:
+            console.log('Martes');
+            break;
+        case 3:
+            console.log('Miércoles');
+            break;
+        case 4:
+            console.log('Jueves');
+            break;
+        case 5:
+            console.log('Viernes');
+            break;
+        case 6:
+            console.log('Sábado');
+            break;
+        case 7:
+            console.log('Domingo');
+            break;
+        default:
+            console.log('Día inválido');
+            break;
+    }
     console.log('Fin del programa');
 })();
--- sections/2.control-structure.ts
+++ sections/2.control-structure.ts
@@ -12,5 +12,33 @@
     else if(grade >= 50) console.log('El alumno debe estudiar más');
     else console.log('El alumno reprobo el curso');
 
+    let weekDay: number = 3;
+    switch(weekDay) {
+        case 1:
+            console.log('Lunes');
+            break;
+        case 2:
+            console.log('Martes');
+            break;
+        case 3:
+            console.log('Miércoles');
+            break;
+        case 4:
+            console.log('Jueves');
+            break;
+        case 5:
+            console.log('Viernes');
+            break;
+        case 6:
+            console.log('Sábado');
+            break;
+        case 7:
+            console.log('Domingo');
+            break;
+        default:
+            console.log('Día inválido');
+            break;
+    }
+
     console.log('Fin del programa');
 })();
