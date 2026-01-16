d8b96bcabc7f291b091d61f29d9b5c413ada8b51
Ricardo Martin Metzger Mejia <analista2@domiruth.com>
2026-01-16 15:29:32 -0500

    if-else

---
+++ app.js
@@ -0,0 +1,16 @@
+(function () {
+    console.log('Inicio del programa');
+    var isTired = true;
+    if (isTired)
+        console.log('Estoy cansado, necesito descansar');
+    else
+        console.log('No estoy cansado, puedo seguir trabajando');
+    var grade = 90;
+    if (grade >= 60)
+        console.log('El alumno aprobó el curso');
+    else if (grade >= 50)
+        console.log('El alumno debe estudiar más');
+    else
+        console.log('El alumno reprobo el curso');
+    console.log('Fin del programa');
+})();
--- sections/1.variables.ts
+++ sections/1.variables.ts
@@ -1,13 +1,15 @@
-let number1: number = 10.5;
-let number2: number = 20.3;
-console.log(number1 + number2);
+(() => {
+    let number1: number = 10.5;
+    let number2: number = 20.3;
+    console.log(number1 + number2);
 
-let firstName: string = "Ricardo";
-let lastName: string = 'Metzger';
-console.log(firstName, lastName);
+    let firstName: string = "Ricardo";
+    let lastName: string = 'Metzger';
+    console.log(firstName, lastName);
 
-let fullName: string = `${firstName} ${lastName}`;
-console.log(fullName);
+    let fullName: string = `${firstName} ${lastName}`;
+    console.log(fullName);
 
-let isActive: boolean = true;
-console.log(isActive);
+    let isActive: boolean = true;
+    console.log(isActive);
+})();
