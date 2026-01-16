e1eb0fa96de67989bd3a5875c54ff057f14b523a
Ricardo Martin Metzger Mejia <analista2@domiruth.com>
2026-01-16 14:43:16 -0500

    init: inicio de proyecto

---
+++ sections/1.variables.ts
@@ -0,0 +1,13 @@
+let number1: number = 10.5;
+let number2: number = 20.3;
+console.log(number1 + number2);
+
+let firstName: string = "Ricardo";
+let lastName: string = 'Metzger';
+console.log(firstName, lastName);
+
+let fullName: string = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let isActive: boolean = true;
+console.log(isActive);
