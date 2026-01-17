daad813371b0e90f44c55b7e6818d338966a9d43
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-16 23:03:36 -0500

    feat: funciones y retornos

--- sections/6.functions.js
+++ sections/6.functions.js
@@ -3,8 +3,13 @@
         if (name === void 0) { name = 'No name'; }
         console.log("Hola ".concat(name));
     }
+    function addTwoNumbers(number1, number2) {
+        return number1 + number2;
+    }
     greet('Ricardo');
     greet();
     greet('Rosa Maria');
     greet('Alvaro Fabrizzio');
+    var total = addTwoNumbers(10, 24);
+    console.log({ total: total });
 })();
--- sections/6.functions.ts
+++ sections/6.functions.ts
@@ -3,8 +3,15 @@
         console.log(`Hola ${name}`);
     }
 
+    function addTwoNumbers(number1: number, number2: number) : number {
+        return number1 + number2;
+    }
+
     greet('Ricardo');
     greet();
     greet('Rosa Maria');
     greet('Alvaro Fabrizzio');
+
+    let total : number = addTwoNumbers(10, 24);
+    console.log({total});
 })();
