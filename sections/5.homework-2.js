c47ddaec2d4fcd7bbe25b84f18081177ef781a2e
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-16 20:07:18 -0500

    feat: tarea de ciclos for

---
+++ sections/5.homework-2.js
@@ -0,0 +1,7 @@
+(function () {
+    var base = 10;
+    var limit = 50;
+    for (var i = 0; i <= limit; i++) {
+        console.log("".concat(base, " x ").concat(i, " = ").concat(base * i));
+    }
+})();
---
+++ sections/5.homework-2.ts
@@ -0,0 +1,8 @@
+(() => {
+    let base = 10;
+    let limit = 50;
+
+    for(let i = 0; i <= limit; i++){
+        console.log(`${base} x ${i} = ${base * i}`);
+    }
+})();
