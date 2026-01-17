c432371905d34d3ebd574ff5ac4413b84518cf5d
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-16 22:36:46 -0500

    feat: tarea con diferentes tipos de ciclos

--- sections/5.homework-2.js
+++ sections/5.homework-2.js
@@ -1,7 +1,21 @@
 (function () {
     var base = 10;
     var limit = 50;
-    for (var i = 0; i <= limit; i++) {
-        console.log("".concat(base, " x ").concat(i, " = ").concat(base * i));
+    var init = 1;
+    console.log('Ciclo For');
+    for (; init <= limit; init++) {
+        console.log("".concat(base, " x ").concat(init, " = ").concat(base * init));
+    }
+    console.log('Ciclo Do-While');
+    init = 1;
+    do {
+        console.log("".concat(base, " x ").concat(init, " = ").concat(base * init));
+        init++;
+    } while (init <= limit);
+    console.log('Ciclo While');
+    init = 1;
+    while (init <= limit) {
+        console.log("".concat(base, " x ").concat(init, " = ").concat(base * init));
+        init++;
     }
 })();
--- sections/5.homework-2.ts
+++ sections/5.homework-2.ts
@@ -1,8 +1,24 @@
 (() => {
     let base = 10;
     let limit = 50;
+    let init = 1;
 
-    for(let i = 0; i <= limit; i++){
-        console.log(`${base} x ${i} = ${base * i}`);
+    console.log('Ciclo For');
+    for(; init <= limit; init++){
+        console.log(`${base} x ${init} = ${base * init}`);
+    }
+
+    console.log('Ciclo Do-While');
+    init = 1;
+    do {
+        console.log(`${base} x ${init} = ${base * init}`);
+        init++;
+    } while (init <= limit);
+
+    console.log('Ciclo While');
+    init = 1;
+    while (init <= limit) {
+        console.log(`${base} x ${init} = ${base * init}`);
+        init++;
     }
 })();
