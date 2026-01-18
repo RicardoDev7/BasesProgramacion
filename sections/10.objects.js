1a5003c23e2d6fb21c7cf7b2e3f0536e888c44fc
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-17 23:43:16 -0500

    feat: tarea de objetos

--- sections/10.objects.js
+++ sections/10.objects.js
@@ -9,10 +9,34 @@
             console.log(message);
         }
     };
+    /*
     console.log(person);
     console.log(person.name);
     console.log(person.name.length);
     console.log(person.name.toUpperCase());
     console.log(person.hobbies[0]);
     person.toString();
+    */
+    var car = {
+        brand: 'Mercedes Benz',
+        seats: 4,
+        color: 'Black',
+        year: 2020
+    };
+    var smartTv = {
+        brand: 'LG',
+        size: 70,
+        color: 'Black',
+        guarantee: 2
+    };
+    var video = {
+        creationDate: '2026-01-15',
+        realeaseDate: '2026-01-07',
+        durationSeconds: 5600,
+        creator: 'Ricardo Metzger',
+        title: 'Clases de programación'
+    };
+    console.log(car);
+    console.log(smartTv);
+    console.log(video);
 })();
--- sections/10.objects.ts
+++ sections/10.objects.ts
@@ -10,10 +10,39 @@
         }
     }
 
+    /*
     console.log(person);
     console.log(person.name);
     console.log(person.name.length);
     console.log(person.name.toUpperCase());
     console.log(person.hobbies[0]);
     person.toString();
+    */
+
+
+    let car = {
+        brand: 'Mercedes Benz',
+        seats: 4,
+        color: 'Black',
+        year: 2020
+    }
+
+    let smartTv = {
+        brand: 'LG',
+        size: 70,
+        color: 'Black',
+        guarantee: 2
+    }
+
+    let video = {
+        creationDate: '2026-01-15',
+        realeaseDate: '2026-01-07',
+        durationSeconds: 5600,
+        creator: 'Ricardo Metzger',
+        title: 'Clases de programación'
+    }
+
+    console.log(car);
+    console.log(smartTv);
+    console.log(video);
 })();
