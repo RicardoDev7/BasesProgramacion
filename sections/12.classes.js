fd5ed831945a6e5db98fd2316bd0f7d8326a5eee
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-18 09:56:58 -0500

    feat: metodos en clases

--- sections/12.classes.js
+++ sections/12.classes.js
@@ -1,15 +1,29 @@
 (function () {
     var Car = /** @class */ (function () {
-        function Car(brand, doors, fuelTank, type) {
+        function Car(brand, doors, type) {
             this.brand = brand;
             this.doors = doors;
-            this.fuelTank = fuelTank;
+            this.fuelTank = 0;
             this.isRunning = false;
             this.type = type;
-            this.creationDate = new Date().getDate();
+            this.creationDate = "".concat(new Date().getDate(), "-").concat(new Date().getUTCMonth() + 1, "-").concat(new Date().getFullYear());
         }
+        Car.prototype.turnOn = function () {
+            if (this.isRunning) {
+                console.log('El carro ya está encendido. Se daño el motor');
+                return;
+            }
+            if (this.fuelTank <= 0) {
+                console.log('El carro no tiene gasolina');
+                return;
+            }
+            this.isRunning = true;
+            console.log('El carro se encendió');
+        };
         return Car;
     }());
-    var myMazda = new Car('Mazda', 4, 100, 'Sedan');
+    var myMazda = new Car('Mazda', 4, 'Sedan');
+    console.log(myMazda);
+    myMazda.turnOn();
     console.log(myMazda);
 })();
--- sections/12.classes.ts
+++ sections/12.classes.ts
@@ -5,17 +5,33 @@
         fuelTank: number;
         isRunning: boolean;
         type: string;
-        creationDate: number;
-        constructor(brand: string, doors: number, fuelTank: number, type: string){
+        creationDate: string;
+        constructor(brand: string, doors: number, type: string){
             this.brand = brand;
             this.doors = doors;
-            this.fuelTank = fuelTank;
+            this.fuelTank = 0;
             this.isRunning = false;
             this.type = type;
-            this.creationDate = new Date().getDate()
+            this.creationDate = `${new Date().getDate()}-${new Date().getUTCMonth() + 1}-${new Date().getFullYear()}`
+        }
+
+        turnOn() : void {
+            if(this.isRunning){
+                console.log('El carro ya está encendido. Se daño el motor');
+                return;
+            }
+            if(this.fuelTank <= 0){
+                console.log('El carro no tiene gasolina');
+                return;
+            }
+
+            this.isRunning = true;
+            console.log('El carro se encendió');
         }
     }
 
-    let myMazda = new Car('Mazda', 4, 100, 'Sedan');
+    let myMazda = new Car('Mazda', 4, 'Sedan');
+    console.log(myMazda);
+    myMazda.turnOn();
     console.log(myMazda);
 })();
