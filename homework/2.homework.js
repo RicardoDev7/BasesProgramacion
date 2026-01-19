62ea9607bebe6cf9f1133bb8154ee30c6d34afde
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-18 19:56:00 -0500

    feat: tarea 2 de programacion

---
+++ homework/2.homework.js
@@ -0,0 +1,9 @@
+(function () {
+    /**
+     * Convertir de kilómetros a millas, sabiendo que: (1 kilómetro es igual a 0.62 millas )
+     * Ejemplo: 10 kilómetros = 6.21 millas
+     */
+    var kilometers = 10;
+    var convertRate = 0.62;
+    console.log("".concat(kilometers, " kil\u00F3metros = ").concat(kilometers * convertRate, " millas"));
+})();
---
+++ homework/2.homework.ts
@@ -0,0 +1,9 @@
+(() => {
+    /**
+     * Convertir de kilómetros a millas, sabiendo que: (1 kilómetro es igual a 0.62 millas )
+     * Ejemplo: 10 kilómetros = 6.21 millas
+     */
+    let kilometers: number = 10;
+    let convertRate: number = 0.62;
+    console.log(`${kilometers} kilómetros = ${kilometers * convertRate} millas`);
+})();
