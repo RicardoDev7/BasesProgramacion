ccd432ed3a074d99e7f57294dd4f6d81cdc93cbe
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-18 21:26:36 -0500

    feat: tarea 6 de programacion

---
+++ homework/6.homework.js
@@ -0,0 +1,17 @@
+(function () {
+    /**
+     * Realice un algoritmo, que permita la impresión en consola de la siguiente tabla:
+     *  1 2 3 4 5
+        2 4 6 8 10
+        3 6 9 12 15
+        4 8 12 16 20
+        5 10 15 20 25
+     */
+    for (var i = 1; i <= 5; i++) {
+        var row = '';
+        for (var j = 1; j <= 5; j++) {
+            row += "".concat(i * j, " ");
+        }
+        console.log(row.trim());
+    }
+})();
